cc70beaac5a5e0dc62de8888d05b785b5deb4ba2
Alfredo Escobar <aescobarcastiglioni@gmail.com>
2025-02-15 13:30:40 -0300

    url and view for facturas data loading

--- backEnd/imcBack/imcAPI/views.py
+++ backEnd/imcBack/imcAPI/views.py
@@ -172,6 +172,41 @@ def upload_partner_file(request):
         logging.error("Invalid request: method=%s, files=%s", request.method, request.FILES)
     return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
 
+@csrf_exempt
+def upload_facturas_file(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        file = request.FILES['file']
+        logging.info("File received: %s", file.name)
+        
+        try:
+            file_path = default_storage.save(os.path.join(settings.MEDIA_ROOT, file.name), file)
+            absolute_file_path = os.path.join(settings.MEDIA_ROOT, file_path)
+            absolute_file_path = Path(absolute_file_path).resolve()
+            logging.info("File saved to: %s", absolute_file_path)
+
+            # Correct the script path based on the actual structure
+            script_path = Path(r'C:\Users\alfre\OneDrive\Documents\Trabajo\IMC\Proyecto IMC\Scripts\data_cleaner\facturas_cleaning_script.py')
+            logging.info("Script path: %s", script_path)
+            venv_python = Path(settings.BASE_DIR) / 'imc-back-env' / 'Scripts' / 'python.exe'
+            venv_python = venv_python.resolve()
+            logging.info("Python interpreter path: %s", venv_python)
+
+            logging.info("Running script: %s with Python interpreter: %s", script_path, venv_python)
+            result = subprocess.run([str(venv_python), str(script_path), str(absolute_file_path)], capture_output=True, text=True)
+
+            if result.returncode == 0:
+                logging.info("Script executed successfully")
+                return JsonResponse({'status': 'success', 'message': 'File uploaded and processed successfully'})
+            else:
+                logging.error("Script execution failed: %s", result.stderr)
+                return JsonResponse({'status': 'error', 'message': result.stderr})
+        except Exception as e:
+            logging.error("Exception occurred: %s", str(e))
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    else:
+        logging.error("Invalid request: method=%s, files=%s", request.method, request.FILES)
+    return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
+
 
 def get_last_10_quotes(request):
     client = bigquery.Client()
