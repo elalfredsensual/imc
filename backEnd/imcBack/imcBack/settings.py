6fa3a2e0460aa58faba963ad37f7c168770db802
Alfredo Escobar <aescobarcastiglioni@gmail.com>
2025-03-02 22:57:19 -0300

    settings url update for prod

--- backEnd/imcBack/imcBack/settings.py
+++ backEnd/imcBack/imcBack/settings.py
@@ -50,12 +50,14 @@ SECRET_KEY = 'django-insecure-(86hnkxihu)*+qxbf)%)49n-mjn8zf#1-2x+2khhcwu7r0v@ea
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['157.245.242.171']
+ALLOWED_HOSTS = ['157.245.242.171',"localhost"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://http://157.245.242.171:5173", 
-    "https://157.245.242.171", 
+    "http://157.245.242.171:5173",
+    "http://157.245.242.171",
+    "https://157.245.242.171"
 ]
+
 
 
 
